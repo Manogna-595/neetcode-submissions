24a8e4d5a10e9cad060f00dbde0d4dc91c37b338
Manogna Kappagantula <manognak595@gmail.com>
2026-08-27 23:30:53 +0530

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.java
@@ -0,0 +1,28 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        intervals.sort((a, b) -> a.start - b.start);
+        if(intervals.size()==0) return true;
+        Interval curr = intervals.get(0);
+
+        for(int i=1;i<intervals.size();i++){
+            Interval next = intervals.get(i);
+            if(next.start >= curr.end){
+                curr = next;
+            } else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
